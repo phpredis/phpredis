04bcecde74eb29df9d5a70a42f7df9f925da801a
Nicolas Favre-Felix <n.favrefelix@gmail.com>
2011-12-19 22:54:53 +0000

    Added missing parameter.

--- redis.c
+++ redis.c
@@ -1974,7 +1974,7 @@ PHPAPI int generic_multiple_args_cmd(INTERNAL_FUNCTION_PARAMETERS, char *keyword
     RedisSock *redis_sock;
 
     if(argc < min_argc) {
-		zend_wrong_param_count();
+		zend_wrong_param_count(TSRMLS_C);
 		ZVAL_BOOL(return_value, 0);
 		return FAILURE;
     }
