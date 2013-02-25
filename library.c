703476ce01490157ef6cf0ed0ccde06714d9e81f
michael-grunder <michael.grunder@gmail.com>
2013-02-24 17:51:56 -0800

    Fix each place we're erroring out but return a value as the methods are int returns
    Resolves issues pertaining to #303

--- library.c
+++ library.c
@@ -1093,7 +1093,7 @@ PHPAPI int redis_sock_read_multibulk_reply(INTERNAL_FUNCTION_PARAMETERS, RedisSo
         IF_MULTI_OR_PIPELINE() {
             add_next_index_bool(z_tab, 0);
         } else {
-            RETURN_FALSE;
+            RETVAL_FALSE;
         }
         return -1;
     }
@@ -1140,7 +1140,7 @@ PHPAPI int redis_sock_read_multibulk_reply_raw(INTERNAL_FUNCTION_PARAMETERS, Red
         IF_MULTI_OR_PIPELINE() {
             add_next_index_bool(z_tab, 0);
         } else {
-            RETURN_FALSE;
+            RETVAL_FALSE;
         }
         return -1;
     }
@@ -1219,7 +1219,7 @@ PHPAPI int redis_sock_read_multibulk_reply_assoc(INTERNAL_FUNCTION_PARAMETERS, R
         IF_MULTI_OR_PIPELINE() {
             add_next_index_bool(z_tab, 0);
         } else {
-            RETURN_FALSE;
+            RETVAL_FALSE;
         }
         return -1;
     }
