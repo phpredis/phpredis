a0e9b65d4ce16eb00a06ea579bf422b8f0493109
michael-grunder <michael.grunder@gmail.com>
2013-09-03 21:17:38 -0700

    Add a unit test for the scenario described in #379

--- tests/TestRedis.php
+++ tests/TestRedis.php
@@ -2282,6 +2282,8 @@ class Redis_Test extends TestSuite
 	$this->assertFalse(array(123 => 'x') === $this->redis->hMget('h', array(123)));
 	$this->assertTrue(array(123 => FALSE) === $this->redis->hMget('h', array(123)));
 
+    // Test with an array populated with things we can't use as keys
+    $this->assertTrue($this->redis->hmget('h', Array(false,NULL,false)) === FALSE);
 
 	// hmget/hmset with numeric fields
 	$this->redis->del('h');
