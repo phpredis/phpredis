50c99f8e6ef274e9534ad0bc907747a4e2211bb1
michael-grunder <michael.grunder@gmail.com>
2014-05-30 10:22:42 -0700

    Initial commit of redis_commands.c
    Given that non cluster and cluster based redis objects should act
    exactly the same way (in terms of argument handling/processing)
    we're extracting the parsing of arguments and command construction
    from being directly inside of the PHP_METHOD itself.
    
    This way, we don't repeat ourselves having two identical places
    where we do this but in a slightly different way.
    
    Every command takes a RedisSock pointer (coming from redis_sock_get
    in a standard redis class, and cluster->flags in a cluster).
    
    If we're passed a pointer for slot, we'll hash one of the keys
    to return it's proper slot.

---
+++ redis_commands.h
@@ -0,0 +1,32 @@
+#ifndef REDIS_COMMANDS_H
+#define REDIS_COMMANDS_H
+
+#include "common.h"
+#include "library.h"
+#include "cluster_library.h"
+
+/* Macro for setting the slot if we've been asked to */
+#define CMD_SET_SLOT(slot,key,key_len) \
+    if(slot) *slot = cluster_hash_key(key,key_len);
+
+/* Redis command construction routines, which always take the form:
+ *      int function(INTERNAL_FUNCTION_PARAMETERS, RedisSock* redis_sock,
+ *                   char **cmd, int *cmd_len, short *slot);
+ *
+ * The functions will return SUCCESS on success, and FAILURE on failure.  In
+ * the case of a failure, the cmd pointer will not have been updated, and
+ * no memory wlll have been allocated (that wasn't freed).
+ *
+ * If the slot pointer is passed as non-null, it will be set to the Redis
+ * Cluster hash slot where the key(s) belong. 
+ */
+
+int redis_get_cmd(INTERNAL_FUNCTION_PARAMETERS, RedisSock *redis_sock,
+                  char **cmd, int *cmd_len, short *slot);
+
+int redis_set_cmd(INTERNAL_FUNCTION_PARAMETERS, RedisSock *redis_sock,
+                  char **cmd, int *cmd_len, short *slot);
+
+#endif
+
+/* vim: set tabstop=4 softtabstops=4 noexpandtab shiftwidth=4: */
