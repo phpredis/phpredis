e1c80864f19e1d8159bad2c17e83def9ac9845f8
michael-grunder <michael.grunder@gmail.com>
2014-05-29 19:04:03 -0700

    Add a 'v' type to redis_cmd_format_static
    Added a new format specifier, 'v', to redis_cmd_format_static
    which stands for 'value', meaning that we can automatically
    serialize the value if our RedisSock* object specifies that
    we should.
    
    This should remove quite a bit of redundant code

--- library.c
+++ library.c
@@ -501,7 +501,9 @@ redis_cmd_format_header(char **ret, char *keyword, int arg_count) {
 }
 
 int
-redis_cmd_format_static(char **ret, char *keyword, char *format, ...) {
+redis_cmd_format_static(char **ret, char *keyword, char *format TSRMLS_DC, 
+                        ...) 
+{
     char *p = format;
     va_list ap;
     smart_str buf = {0};
@@ -542,6 +544,16 @@ redis_cmd_format_static(char **ret, char *keyword, char *format, ...) {
                 smart_str_appendl(&buf, _NL, sizeof(_NL)-1);
                 smart_str_appendl(&buf,key,key_len);
                 if(key_free) efree(key);
+                }
+                break;
+            case 'v': {
+                char *zval = va_arg(ap,zval*);
+                RedisSock *redis_sock = va_arg(ap,RedisSock*);
+                int val_free = redis_serialize(redis_sock,&val,&val_len TSRMLS_CC);
+                smart_str_append_long(&buf,val_len);
+                smart_str_appendl(&buf, _NL, sizeof(_NL) - 1);
+                smart_str_appendl(&buf,val,val_len);
+                if(val_free) STR_FREE(val);
                 }
                 break;
 			case 'f':
