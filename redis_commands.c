72940eb986f74db06d0594b17e8da662caca7622
michael-grunder <michael.grunder@gmail.com>
2014-05-30 10:22:42 -0700

    Initial commit of redis_commands.c
    Given that non cluster and cluster based redis objects should act
    exactly the same way (in terms of argument handling/processing)
    we're extracting the parsing of arguments and command construction
    from being directly inside of the PHP_METHOD itself.
    
    This way, we don't repeat ourselves having two identical places
    where we do this but in a slightly different way.
    
    Every command takes a RedisSock pointer (coming from redis_sock_get
    in a standard redis class, and cluster->flags in a cluster).
    
    If we're passed a pointer for slot, we'll hash one of the keys
    to return it's proper slot.

---
+++ redis_commands.c
@@ -0,0 +1,129 @@
+/* -*- Mode: C; tab-width: 4 -*- */
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2009 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Original Author: Michael Grunder <michael.grunder@gmail.com          |
+  | Maintainer: Nicolas Favre-Felix <n.favre-felix@owlient.eu>           |
+  +----------------------------------------------------------------------+
+*/
+
+#include "redis_commands.h"
+
+/* GET */
+int redis_get_cmd(INTERNAL_FUNCTION_PARAMETERS, RedisSock *redis_sock,
+                  char **cmd, int *cmd_len, short *slot)
+{
+    char *key=NULL;
+    int key_len, key_free;
+
+    if(zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &key, 
+                             &key_len)==FAILURE)
+    {
+        return FAILURE;
+    }
+
+    key_free = redis_key_prefix(redis_sock, &key, &key_len);
+    *cmd_len = redis_cmd_format_static(cmd, "GET", "s", key, key_len);
+    
+    CMD_SET_SLOT(slot, key, key_len);
+    if(key_free) efree(key);
+
+    return SUCCESS;
+}
+
+/* SET */
+int redis_set_cmd(INTERNAL_FUNCTION_PARAMETERS, RedisSock *redis_sock,
+                  char **cmd, int *cmd_len, short *slot)
+{
+    zval *z_value, *z_opts=NULL;
+    char *key = NULL, *val = NULL, *exp_type = NULL, *set_type = NULL;
+    int key_len, val_len, key_free, val_free;
+    long expire = -1;
+
+    // Make sure the function is being called correctly
+    if(zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sz|z", &key, &key_len,
+                             &z_value, &z_opts)==FAILURE)
+    {
+        return FAILURE;
+    }
+
+    // Serialize and key prefix if required
+    val_free = redis_serialize(redis_sock, z_value, &val, &val_len TSRMLS_CC);
+    key_free = redis_key_prefix(redis_sock, &key, &key_len);
+
+    // Check for an options array
+    if(z_opts && Z_TYPE_P(z_opts) == IS_ARRAY) {
+        HashTable *kt = Z_ARRVAL_P(z_opts);
+        int type;
+        unsigned int ht_key_len;
+        unsigned long idx;
+        char *k;
+        zval **v;
+
+        /* Iterate our option array */
+        for(zend_hash_internal_pointer_reset(kt);
+            zend_hash_has_more_elements(kt) == SUCCESS;
+            zend_hash_move_forward(kt))
+        {
+            // Grab key and value
+            type = zend_hash_get_current_key_ex(kt, &k, &ht_key_len, &idx, 0, NULL);
+            zend_hash_get_current_data(kt, (void**)&v);
+
+            if(type == HASH_KEY_IS_STRING && (Z_TYPE_PP(v) == IS_LONG) &&
+               (Z_LVAL_PP(v) > 0) && IS_EX_PX_ARG(k))
+            {
+                exp_type = k;
+                expire = Z_LVAL_PP(v);
+            } else if(Z_TYPE_PP(v) == IS_STRING && IS_NX_XX_ARG(Z_STRVAL_PP(v))) {
+                set_type = Z_STRVAL_PP(v);
+            }
+        }
+    } else if(z_opts && Z_TYPE_P(z_opts) == IS_LONG) {
+        expire = Z_LVAL_P(z_opts);
+    }
+
+    /* Now let's construct the command we want */
+    if(exp_type && set_type) {
+        /* SET <key> <value> NX|XX PX|EX <timeout> */
+        *cmd_len = redis_cmd_format_static(cmd, "SET", "ssssl", key, key_len, 
+                                           val, val_len, set_type, 2, exp_type, 
+                                           2, expire);
+    } else if(exp_type) {
+        /* SET <key> <value> PX|EX <timeout> */
+        *cmd_len = redis_cmd_format_static(cmd, "SET", "sssl", key, key_len, 
+                                           val, val_len, exp_type, 2, expire);
+    } else if(set_type) {
+        /* SET <key> <value> NX|XX */
+        *cmd_len = redis_cmd_format_static(cmd, "SET", "sss", key, key_len, val,
+                                           val_len, set_type, 2);
+    } else if(expire > 0) {
+        /* Backward compatible SETEX redirection */
+        *cmd_len = redis_cmd_format_static(cmd, "SETEX", "sls", key, key_len, 
+                                           expire, val, val_len);
+    } else {
+        /* SET <key> <value> */
+        *cmd_len = redis_cmd_format_static(cmd, "SET", "ss", key, key_len, val, 
+                                          val_len);
+    }
+
+    // If we've been passed a slot pointer, return the key's slot
+    CMD_SET_SLOT(slot,key,key_len);
+
+    if(key_free) efree(key);
+    if(val_free) efree(val);
+
+    return SUCCESS;
+}
+
+/* vim: set tabstop=4 softtabstops=4 noexpandtab shiftwidth=4: */
