dee14d7dee1a359bc81e4f1539180e5294faefa6
michael-grunder <michael.grunder@gmail.com>
2014-05-31 13:03:39 -0700

    Added EOF as a REDIS_REPLY_TYPE error state
    This allows us to check that we don't get EOF when getting a
    single character on the stream, while still pulling into a
    REDIS_REPLY_TYPE variable.

--- common.h
+++ common.h
@@ -30,7 +30,7 @@
 
 /* reply types */
 typedef enum _REDIS_REPLY_TYPE {
-    TYPE_EOF       = EOF,
+    TYPE_EOF       = -1,
     TYPE_LINE      = '+',
 	TYPE_INT       = ':',
 	TYPE_ERR       = '-',
