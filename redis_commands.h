04b20bef8cc2f45e1038cad0780c22b70266d22b
michael-grunder <michael.grunder@gmail.com>
2014-06-14 09:30:18 -0700

    UNSUBSCRIBE/PUNSUBSCRIBE
    Implemented the two unsubscribe commands in Redis Cluster.
    Presently, like with the standard Redis class once you subscribe
    you're there for good, but it would be nice to be able to use the
    callback return value to break out of the subscribe loop.

--- redis_commands.h
+++ redis_commands.h
@@ -82,6 +82,9 @@ int redis_zinter_cmd(INTERNAL_FUNCTION_PARAMETERS, RedisSock *redis_sock,
 int redis_subscribe_cmd(INTERNAL_FUNCTION_PARAMETERS, RedisSock *redis_sock,
     char *kw, char **cmd, int *cmd_len, short *slot, void **ctx);
 
+int redis_unsubscribe_cmd(INTERNAL_FUNCTION_PARAMETERS, RedisSock *redis_sock,
+    char *kw, char **cmd, int *cmd_len, short *slot, void **ctx);
+
 /* Commands which need a unique construction mechanism.  This is either because
  * they don't share a signature with any other command, or because there is 
  * specific processing we do (e.g. verifying subarguments) that make them
