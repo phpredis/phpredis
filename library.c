9499949b1db7dbcf21e03d07110ab8cc4a323e6e
Anatol Belski <ab@php.net>
2014-07-01 11:29:30 +0200

    fix random() vs php_rand() for portability
    Conflicts:
    	library.c

--- library.c
+++ library.c
@@ -187,12 +187,9 @@ PHP_REDIS_API int redis_check_eof(RedisSock *redis_sock TSRMLS_DC)
     }
     // Wait for a while before trying to reconnect
     if (redis_sock->retry_interval) {
-        // Random factor to avoid having several (or many) concurrent 
-        // connections trying to reconnect at the same time
-        long retry_interval = (count ? redis_sock->retry_interval 
-            : (random() % redis_sock->retry_interval));
-        
-        usleep(retry_interval);
+    	// Random factor to avoid having several (or many) concurrent connections trying to reconnect at the same time
+   		long retry_interval = (count ? redis_sock->retry_interval : (php_rand(TSRMLS_C) % redis_sock->retry_interval));
+    	usleep(retry_interval);
     }
         redis_sock_connect(redis_sock TSRMLS_CC); /* reconnect */
         if(redis_sock->stream) { /*  check for EOF again. */
