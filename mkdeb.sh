982b63b976f8c98bb32ed07414194d4b2863ecb6
Vladimir <arkhamvm@users.noreply.github.com>
2016-07-20 13:27:16 +1000

    Update mkdeb.sh for PHP 7.0

--- mkdeb.sh
+++ mkdeb.sh
@@ -13,8 +13,8 @@ mkdir -p debian/$DIR
 cp debian.control debian/DEBIAN/control
 
 UBUNTU=`uname -v | grep -ci ubuntu`
-mkdir -p debian/etc/php5/conf.d/
-echo "extension=redis.so" >> debian/etc/php5/conf.d/redis.ini
+mkdir -p debian/etc/php/7.0/conf.d/
+echo "extension=redis.so" >> debian/etc/php/7.0/conf.d/redis.ini
 
 cp modules/redis.so debian/$DIR
 dpkg -b debian phpredis-`git describe --abbrev=0 --tags`_`uname -m`.deb
