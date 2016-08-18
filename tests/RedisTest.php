bbf98f49941f1156bcbea1c1df7e33ef6e4d018f
Daniel Bachhuber <daniel@handbuilt.co>
2016-08-18 10:03:03 -0700

    Add a test case for `resetStat()` method

--- tests/RedisTest.php
+++ tests/RedisTest.php
@@ -4944,5 +4944,9 @@ class Redis_Test extends TestSuite
         $this->redis->rpush('mylist', 'A', 'B', 'C', 'D');
         $this->assertEquals($this->redis->lrange('mylist', 0, -1), Array('A','B','C','D'));
     }
+
+    public function testResetStatCommand() {
+        $this->assertTrue( $this->redis->resetStat() );
+    }
 }
 ?>
