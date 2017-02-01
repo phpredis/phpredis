04778ef10c17b5e803233feb8b301b4556b3fcaa
Pavlo Yatsukhnenko <yatsukhnenko@gmail.com>
2017-02-01 10:03:44 +0200

    3.1.1

--- php_redis.h
+++ php_redis.h
@@ -25,7 +25,7 @@
 #define PHP_REDIS_H
 
 /* phpredis version */
-#define PHP_REDIS_VERSION "3.1.1RC1"
+#define PHP_REDIS_VERSION "3.1.1"
 
 PHP_METHOD(Redis, __construct);
 PHP_METHOD(Redis, __destruct);
