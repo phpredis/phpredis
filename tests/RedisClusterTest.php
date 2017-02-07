d9ef807b1cae1c29ca07a0511e2d152e50e80228
Alberto Fernández <albertofem@gmail.com>
2017-02-07 22:43:10 +0100

    Failing test case when running LUA with bulk empty response

--- tests/RedisClusterTest.php
+++ tests/RedisClusterTest.php
@@ -346,6 +346,20 @@ class Redis_Cluster_Test extends Redis_Test {
         $this->assertTrue(1 === $this->redis->evalsha($sha,Array($str_key), 1));
     }
 
+    public function testEvalBulkEmptyResponse() {
+        $str_key1 = uniqid() . '-' . rand(1,1000) . '{hash}';
+        $str_key2 = uniqid() . '-' . rand(1,1000) . '{hash}';
+
+        $this->redis->script($str_key1, 'flush');
+        $this->redis->script($str_key2, 'flush');
+
+        $scr = "for _,key in ipairs(KEYS) do redis.call('SET', key, 'value') end";
+
+        $result = $this->redis->eval($scr,Array($str_key1, $str_key2), 2);
+
+        $this->assertTrue(null === $result);
+    }
+
     /* Cluster specific introspection stuff */
     public function testIntrospection() {
         $arr_masters = $this->redis->_masters();
