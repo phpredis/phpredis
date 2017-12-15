6733edce801e994cb6c7798371b53aa05eb9a2e0
Frank M. Kromann <fmk@php.net>
2017-12-14 16:06:46 -0800

    Swap logic to catch PHP 5.x builds

--- redis_session.c
+++ redis_session.c
@@ -231,10 +231,10 @@ PS_OPEN_FUNC(redis)
             if (url->query != NULL) {
                 array_init(&params);
 
-#if (PHP_MAJOR_VERSION == 7) && (PHP_MINOR_VERSION < 3)
-                sapi_module.treat_data(PARSE_STRING, estrdup(url->query), &params TSRMLS_CC);
-#else
+#if (PHP_MAJOR_VERSION == 7) && (PHP_MINOR_VERSION > 2)
                 sapi_module.treat_data(PARSE_STRING, estrdup(ZSTR_VAL(url->query)), &params TSRMLS_CC);
+#else
+                sapi_module.treat_data(PARSE_STRING, estrdup(url->query), &params TSRMLS_CC);
 #endif
 
                 if ((param = zend_hash_str_find(Z_ARRVAL(params), "weight", sizeof("weight") - 1)) != NULL) {
@@ -280,16 +280,16 @@ PS_OPEN_FUNC(redis)
 
             RedisSock *redis_sock;
             if(url->host) {
-#if (PHP_MAJOR_VERSION == 7) && (PHP_MINOR_VERSION < 3)
-                redis_sock = redis_sock_create(url->host, strlen(url->host), url->port, timeout, read_timeout, persistent, persistent_id, retry_interval, 0);
-#else
+#if (PHP_MAJOR_VERSION == 7) && (PHP_MINOR_VERSION > 2)
                 redis_sock = redis_sock_create(ZSTR_VAL(url->host), ZSTR_LEN(url->host), url->port, timeout, read_timeout, persistent, persistent_id, retry_interval, 0);
+#else
+                redis_sock = redis_sock_create(url->host, strlen(url->host), url->port, timeout, read_timeout, persistent, persistent_id, retry_interval, 0);
 #endif
             } else { /* unix */
-#if (PHP_MAJOR_VERSION == 7) && (PHP_MINOR_VERSION < 3)
-                redis_sock = redis_sock_create(url->path, strlen(url->path), 0, timeout, read_timeout, persistent, persistent_id, retry_interval, 0);
-#else
+#if (PHP_MAJOR_VERSION == 7) && (PHP_MINOR_VERSION > 2)
                 redis_sock = redis_sock_create(ZSTR_VAL(url->path), ZSTR_LEN(url->path), 0, timeout, read_timeout, persistent, persistent_id, retry_interval, 0);
+#else
+                redis_sock = redis_sock_create(url->path, strlen(url->path), 0, timeout, read_timeout, persistent, persistent_id, retry_interval, 0);
 #endif
             }
             redis_pool_add(pool, redis_sock, weight, database, prefix, auth TSRMLS_CC);
