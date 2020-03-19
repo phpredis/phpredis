650ac0560d912c7e32dc75c9080e6aad91cab2f0
michael-grunder <michael.grunder@gmail.com>
2020-03-19 10:31:43 -0700

    Bump version in php_redis.h

--- php_redis.h
+++ php_redis.h
@@ -23,7 +23,7 @@
 #define PHP_REDIS_H
 
 /* phpredis version */
-#define PHP_REDIS_VERSION "5.2.0"
+#define PHP_REDIS_VERSION "5.2.1"
 
 PHP_METHOD(Redis, __construct);
 PHP_METHOD(Redis, __destruct);
