d7356147326364fcc35bd79ef93243e74b10b9a2
Ilia Alshanetsky <ilia@ilia.ws>
2020-05-10 13:12:37 -0400

    fixed checks

--- library.c
+++ library.c
@@ -2367,13 +2367,16 @@ redis_unpack(RedisSock *redis_sock, const char *val, int val_len, zval *z_ret)
 #ifdef HAVE_REDIS_ZSTD
             {
                 char *data;
-                size_t len;
+                long long len;
 
-                if (redis_sock->compression_min_size > 0 && !ZSTD_isFrame(val, val_len)) {
+                len = ZSTD_getFrameContentSize(val, val_len);
+                if (
+                    (redis_sock->compression_min_ratio > 0 || redis_sock->compression_min_size > 0)
+                    && (len == ZSTD_CONTENTSIZE_UNKNOWN || len == ZSTD_CONTENTSIZE_ERROR || len <= 0)
+                ) {
                     return redis_unserialize(redis_sock, val, val_len, z_ret);
                 }
 
-                len = ZSTD_getFrameContentSize(val, val_len);
                 if (len >= 0) {
                     data = emalloc(len);
                     len = ZSTD_decompress(data, len, val, val_len);
