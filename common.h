e2f59703bb14678caade238200eb2f105f162c19
michael-grunder <michael.grunder@gmail.com>
2020-05-23 14:00:23 -0700

    More consistent naming

--- common.h
+++ common.h
@@ -218,14 +218,14 @@ typedef enum {
     REDIS_STRICMP_STATIC(Z_STRVAL_P(zv), Z_STRLEN_P(zv), sstr)
 
 /* Case insensitive compare of a zend_string to a static string */
-#define ZSTR_CMP_STATIC(zs, sstr) \
+#define ZSTR_STRICMP_STATIC(zs, sstr) \
     REDIS_STRICMP_STATIC(ZSTR_VAL(zs), ZSTR_LEN(zs), sstr)
 
 /* Extended SET argument detection */
-#define ZSTR_IS_EX_ARG(zs) ZSTR_CMP_STATIC(zs, "EX")
-#define ZSTR_IS_PX_ARG(zs) ZSTR_CMP_STATIC(zs, "PX")
-#define ZSTR_IS_NX_ARG(zs) ZSTR_CMP_STATIC(zs, "NX")
-#define ZSTR_IS_XX_ARG(zs) ZSTR_CMP_STATIC(zs, "XX")
+#define ZSTR_IS_EX_ARG(zs) ZSTR_STRICMP_STATIC(zs, "EX")
+#define ZSTR_IS_PX_ARG(zs) ZSTR_STRICMP_STATIC(zs, "PX")
+#define ZSTR_IS_NX_ARG(zs) ZSTR_STRICMP_STATIC(zs, "NX")
+#define ZSTR_IS_XX_ARG(zs) ZSTR_STRICMP_STATIC(zs, "XX")
 
 #define ZVAL_IS_NX_XX_ARG(zv) (ZVAL_STRICMP_STATIC(zv, "NX") || ZVAL_STRICMP_STATIC(zv, "XX"))
 #define ZSTR_IS_EX_PX_ARG(a) (ZSTR_IS_EX_ARG(a) || ZSTR_IS_PX_ARG(a))
