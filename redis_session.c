149115ef426bfd5ef0dfb26501919ee6b243345e
michael-grunder <michael.grunder@gmail.com>
2020-07-01 15:24:36 -0700

    Don't attempt to take ZSTR_VAL(NULL)
    Fixes #1798

--- redis_session.c
+++ redis_session.c
@@ -480,7 +480,8 @@ PS_OPEN_FUNC(redis)
             }
 
             redis_sock = redis_sock_create(addr, addrlen, port, timeout, read_timeout,
-                                           persistent, ZSTR_VAL(persistent_id), retry_interval);
+                                           persistent, persistent_id ? ZSTR_VAL(persistent_id) : NULL,
+                                           retry_interval);
 
             redis_pool_add(pool, redis_sock, weight, db);
             redis_sock->prefix = prefix;
