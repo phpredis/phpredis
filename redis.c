e2a22c6960bfb12b9cc2926c5d7153b34ce812de
Aurélien LAJOIE <aurelien.lajoie@manomano.com>
2021-02-04 17:34:14 +0100

    Fix connect proto reserved was missing

--- redis.c
+++ redis.c
@@ -978,7 +978,8 @@ PHP_METHOD(Redis,__destruct) {
     }
 }
 
-/* {{{ proto boolean Redis::connect(string host, int port [, double timeout [, long retry_interval]])
+/* {{{ proto boolean Redis::connect(string host, int port
+ * [, double timeout [, NULL reserved, long retry_interval [, float read_timeout]]])
  */
 PHP_METHOD(Redis, connect)
 {
@@ -990,7 +991,8 @@ PHP_METHOD(Redis, connect)
 }
 /* }}} */
 
-/* {{{ proto boolean Redis::pconnect(string host, int port [, double timeout])
+/* {{{ proto boolean Redis::pconnect(string host, int port
+ * [, double timeout[, string persistent_id, long retry_interval [, float read_timeout]]])
  */
 PHP_METHOD(Redis, pconnect)
 {
