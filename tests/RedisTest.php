a43f4586ef7550321f05ec4c6514b29714445f90
Pavlo Yatsukhnenko <yatsukhnenko@gmail.com>
2021-03-06 12:38:56 +0200

    TravisCI: fix tests

--- tests/RedisTest.php
+++ tests/RedisTest.php
@@ -6168,7 +6168,6 @@ class Redis_Test extends TestSuite
         $obj_new = $this->newInstance();
 
         $arr_args = [
-            NULL,
             [],
             [NULL, NULL],
             ['foo', 'bar', 'baz'],
@@ -6184,8 +6183,6 @@ class Redis_Test extends TestSuite
             try {
                 if (is_array($arr_arg)) {
                     @call_user_func_array([$obj_new, 'auth'], $arr_arg);
-                } else {
-                    @call_user_func([$obj_new, 'auth']);
                 }
             } catch (Exception $ex) {
                 unset($ex); /* Suppress intellisense warning */
