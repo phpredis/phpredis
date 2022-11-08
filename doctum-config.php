142bddf0b71c758a0e2d8930277b61c015ed8322
Till Krüss <tillkruss@users.noreply.github.com>
2022-11-08 14:59:24 -0800

    Add initial docs (#2249)
    Initial support for `doctum` generated documentation.

---
+++ doctum-config.php
@@ -0,0 +1,28 @@
+<?php
+
+use Doctum\Doctum;
+use Doctum\Version\GitVersionCollection;
+use Doctum\RemoteRepository\GitHubRemoteRepository;
+
+use Symfony\Component\Finder\Finder;
+
+$root = realpath(__DIR__);
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.stub.php')
+    ->in($root);
+
+$versions = GitVersionCollection::create($root)
+    ->add('develop', 'develop');
+
+return new Doctum($iterator, [
+    'title' => 'PhpRedis API',
+    'language' => 'en',
+    'source_dir' => $root,
+    'build_dir' => "{$root}/docs",
+    'cache_dir' => "{$root}/docs/.cache",
+    'base_url' => 'https://phpredis.github.io/',
+    'versions' => $versions,
+    'remote_repository' => new GitHubRemoteRepository('phpredis/phpredis', $root),
+]);
---
+++ doctum.md
@@ -0,0 +1,8 @@
+# API Documentation
+
+```bash
+curl -O https://doctum.long-term.support/releases/latest/doctum.phar
+chmod +x doctum.phar
+
+./doctum.phar update doctum-config.php
+```
