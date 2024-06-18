9f379855f437d07e4824427a8102902207f415ca
michael-grunder <michael.grunder@gmail.com>
2024-06-18 15:41:26 -0700

    PHP might throw a fatal error if we send no args to exists

--- tests/RedisTest.php
+++ tests/RedisTest.php
@@ -939,7 +939,8 @@ class Redis_Test extends TestSuite {
 
         /* Test passing an array as well as the keys variadic */
         $this->assertEquals(count($mkeys), $this->redis->exists($mkeys));
-        $this->assertEquals(count($mkeys), $this->redis->exists(...$mkeys));
+        if (count($mkeys))
+            $this->assertEquals(count($mkeys), $this->redis->exists(...$mkeys));
     }
 
     public function testTouch() {
