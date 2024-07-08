7de29d57d919f835f902f87a83312ed2549c1a13
michael-grunder <michael.grunder@gmail.com>
2024-07-07 19:20:36 -0700

    Fix a macOS (M1) compiler warning.

--- library.c
+++ library.c
@@ -3130,7 +3130,7 @@ PHP_REDIS_API int redis_sock_connect(RedisSock *redis_sock)
             }
 
             gettimeofday(&tv, NULL);
-            persistent_id = strpprintf(0, "phpredis_%ld%ld", tv.tv_sec, tv.tv_usec);
+            persistent_id = strpprintf(0, "phpredis_%ld%ld", tv.tv_sec, (long)tv.tv_usec);
         } else {
             if (redis_sock->persistent_id) {
                 persistent_id = strpprintf(0, "phpredis:%s:%s", host, ZSTR_VAL(redis_sock->persistent_id));
