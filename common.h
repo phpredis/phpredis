90705a9ab2468d5d8072166c66c5e04eddaf6c96
michael-grunder <michael.grunder@gmail.com>
2025-08-15 09:25:42 -0700

    Rework `REDIS_PROCESS_CMD` into a static function

--- common.h
+++ common.h
@@ -220,22 +220,6 @@ typedef enum {
     REDIS_PROCESS_RESPONSE_CLOSURE(function, NULL) \
 }
 
-/* Process a command assuming our command where our command building
- * function is redis_<cmdname>_cmd */
-#define REDIS_PROCESS_CMD(cmdname, resp_func) \
-    RedisSock *redis_sock; char *cmd; int cmd_len; void *ctx=NULL; \
-    if ((redis_sock = redis_sock_get(getThis(), 0)) == NULL || \
-       redis_##cmdname##_cmd(INTERNAL_FUNCTION_PARAM_PASSTHRU,redis_sock, \
-                             &cmd, &cmd_len, NULL, &ctx)==FAILURE) { \
-            RETURN_FALSE; \
-    } \
-    REDIS_PROCESS_REQUEST(redis_sock, cmd, cmd_len); \
-    if (IS_ATOMIC(redis_sock)) { \
-        resp_func(INTERNAL_FUNCTION_PARAM_PASSTHRU, redis_sock, NULL, ctx); \
-    } else { \
-        REDIS_PROCESS_RESPONSE_CLOSURE(resp_func, ctx) \
-    }
-
 /* Process a command but with a specific command building function
  * and keyword which is passed to us*/
 #define REDIS_PROCESS_KW_CMD(kw, cmdfunc, resp_func) \
