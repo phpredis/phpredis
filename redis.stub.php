4a3895115ec8af7484463909ee422543aabb9559
michael-grunder <michael.grunder@gmail.com>
2025-11-09 09:42:25 -0800

    docs: Syntax highlight ```php ... ``` code blocks

--- redis.stub.php
+++ redis.stub.php
@@ -470,42 +470,44 @@ class Redis {
      *
      * **NOTE**:  Below is an example options array with various setting
      *
-     *     $options = [
-     *         'host'           => 'localhost',
-     *         'port'           => 6379,
-     *         'readTimeout'    => 2.5,
-     *         'connectTimeout' => 2.5,
-     *         'persistent'     => true,
+     *```php
+     *$options = [
+     *    'host'           => 'localhost',
+     *    'port'           => 6379,
+     *    'readTimeout'    => 2.5,
+     *    'connectTimeout' => 2.5,
+     *    'persistent'     => true,
      *
-     *         // Valid formats: NULL, ['user', 'pass'], 'pass', or ['pass']
-     *         'auth' => ['phpredis', 'phpredis'],
+     *    // Valid formats: NULL, ['user', 'pass'], 'pass', or ['pass']
+     *    'auth' => ['phpredis', 'phpredis'],
      *
-     *         // See PHP stream options for valid SSL configuration settings.
-     *         'ssl' => ['verify_peer' => false],
+     *    // See PHP stream options for valid SSL configuration settings.
+     *    'ssl' => ['verify_peer' => false],
      *
-     *         // How quickly to retry a connection after we time out or it  closes.
-     *         // Note that this setting is overridden by 'backoff' strategies.
-     *         'retryInterval'  => 100,
+     *    // How quickly to retry a connection after we time out or it  closes.
+     *    // Note that this setting is overridden by 'backoff' strategies.
+     *    'retryInterval'  => 100,
      *
-     *          // Which backoff algorithm to use.  'decorrelated jitter' is
-     *          // likely the best one for most solution, but there are many
-     *          // to choose from:
-     *          //     REDIS_BACKOFF_ALGORITHM_DEFAULT
-     *          //     REDIS_BACKOFF_ALGORITHM_CONSTANT
-     *          //     REDIS_BACKOFF_ALGORITHM_UNIFORM
-     *          //     REDIS_BACKOFF_ALGORITHM_EXPONENTIAL
-     *          //     REDIS_BACKOFF_ALGORITHM_FULL_JITTER
-     *          //     REDIS_BACKOFF_ALGORITHM_EQUAL_JITTER
-     *          //     REDIS_BACKOFF_ALGORITHM_DECORRELATED_JITTER
-     *          // 'base', and 'cap' are in milliseconds and represent the first
-     *          // delay redis will use when reconnecting, and the maximum delay
-     *          // we will reach while retrying.
-     *         'backoff' => [
-     *             'algorithm' => Redis::BACKOFF_ALGORITHM_DECORRELATED_JITTER,
-     *             'base'      => 500,
-     *             'cap'       => 750,
-     *         ]
-     *     ];
+     *     // Which backoff algorithm to use.  'decorrelated jitter' is
+     *     // likely the best one for most solution, but there are many
+     *     // to choose from:
+     *     //     REDIS_BACKOFF_ALGORITHM_DEFAULT
+     *     //     REDIS_BACKOFF_ALGORITHM_CONSTANT
+     *     //     REDIS_BACKOFF_ALGORITHM_UNIFORM
+     *     //     REDIS_BACKOFF_ALGORITHM_EXPONENTIAL
+     *     //     REDIS_BACKOFF_ALGORITHM_FULL_JITTER
+     *     //     REDIS_BACKOFF_ALGORITHM_EQUAL_JITTER
+     *     //     REDIS_BACKOFF_ALGORITHM_DECORRELATED_JITTER
+     *     // 'base', and 'cap' are in milliseconds and represent the first
+     *     // delay redis will use when reconnecting, and the maximum delay
+     *     // we will reach while retrying.
+     *    'backoff' => [
+     *        'algorithm' => Redis::BACKOFF_ALGORITHM_DECORRELATED_JITTER,
+     *        'base'      => 500,
+     *        'cap'       => 750,
+     *    ]
+     *];
+     *```
      *
      * Note: If you do wish to connect via the constructor, only 'host' is
      *       strictly required, which will cause PhpRedis to connect to that
