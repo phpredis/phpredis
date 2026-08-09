KentarouTakeda/phpredis-phpredis

Reset server-side state when reusing persistent connections

library.c
@@ -3273,7 +3273,7 @@ redis_check_echo_response(RedisSock *redis_sock, char *hdr, const char *id,
 }
 
 static int
-redis_sock_check_liveness(RedisSock *redis_sock)
+redis_sock_check_liveness(RedisSock *redis_sock, ConnectionPool *p)
 {
     char id[64], inbuf[256] = {0};
     int idlen, auth;
@@ -3294,6 +3294,12 @@ redis_sock_check_liveness(RedisSock *redis_sock)
         return SUCCESS;
     }
 
+    /* RESET server-side state (Redis 6.2+, skipped once known unsupported) */
+    zend_bool send_reset = (p && !p->reset_unsupported);
+    if (send_reset) {
+        smart_string_appendl(&cmd, RESP_RESET_CMD, sizeof(RESP_RESET_CMD) - 1);
+    }
+
     /* AUTH (if we need it) */
     auth = redis_sock_append_auth(redis_sock, &cmd);
 
@@ -3308,6 +3314,20 @@ redis_sock_check_liveness(RedisSock *redis_sock)
         goto failure;
     }
     smart_string_free(&cmd);
+
+    /* Consume RESET response: +RESET on 6.2+, -ERR on older Redis */
+    if (send_reset) {
+        if (redis_sock_gets(redis_sock, inbuf, sizeof(inbuf) - 1, &len) < 0) {
+            goto failure;
+        }
+        if (redis_strncmp(inbuf, ZEND_STRL("+RESET")) != 0) {
+            if (redis_strncmp(inbuf, ZEND_STRL("-ERR")) == 0) {
+                p->reset_unsupported = 1;
+            } else {
+                goto failure;
+            }
+        }
+    }
 
     if (redis_sock_gets(redis_sock, inbuf, sizeof(inbuf) - 1, &len) < 0) {
         goto failure;
@@ -3425,7 +3445,7 @@ PHP_REDIS_API int redis_sock_connect(RedisSock *redis_sock)
                 redis_sock->stream = *(php_stream **)zend_llist_get_last(&p->list);
                 zend_llist_remove_tail(&p->list);
 
-                if (redis_sock_check_liveness(redis_sock) == SUCCESS) {
+                if (redis_sock_check_liveness(redis_sock, p) == SUCCESS) {
                     return SUCCESS;
                 }
 

phpredis/phpredis

library.c
@@ -3135,7 +3135,7 @@ redis_check_echo_response(RedisSock *redis_sock, char *hdr, const char *id,
 }
 
 static int
-redis_sock_check_liveness(RedisSock *redis_sock)
+redis_sock_check_liveness(RedisSock *redis_sock, ConnectionPool *p)
 {
     char id[64], inbuf[256] = {0};
     smart_str cmd = {0};
@@ -3156,6 +3156,12 @@ redis_sock_check_liveness(RedisSock *redis_sock)
         return SUCCESS;
     }
 
+    /* RESET server-side state (Redis 6.2+, skipped once known unsupported) */
+    zend_bool send_reset = (p && !p->reset_unsupported);
+    if (send_reset) {
+        smart_str_appendl(&cmd, RESP_RESET_CMD, sizeof(RESP_RESET_CMD) - 1);
+    }
+
     /* AUTH (if we need it) */
     auth = redis_sock_append_auth(redis_sock, &cmd);
 
@@ -3172,6 +3178,20 @@ redis_sock_check_liveness(RedisSock *redis_sock)
     }
 
     smart_str_free(&cmd);
+
+    /* Consume RESET response: +RESET on 6.2+, -ERR on older Redis */
+    if (send_reset) {
+        if (redis_sock_gets(redis_sock, inbuf, sizeof(inbuf) - 1, &len) < 0) {
+            goto failure;
+        }
+        if (redis_strncmp(inbuf, ZEND_STRL("+RESET")) != 0) {
+            if (redis_strncmp(inbuf, ZEND_STRL("-ERR")) == 0) {
+                p->reset_unsupported = 1;
+            } else {
+                goto failure;
+            }
+        }
+    }
 
     if (redis_sock_gets(redis_sock, inbuf, sizeof(inbuf) - 1, &len) < 0) {
         goto failure;
@@ -3289,7 +3309,7 @@ PHP_REDIS_API int redis_sock_connect(RedisSock *redis_sock)
                 redis_sock->stream = *(php_stream **)zend_llist_get_last(&p->list);
                 zend_llist_remove_tail(&p->list);
 
-                if (redis_sock_check_liveness(redis_sock) == SUCCESS) {
+                if (redis_sock_check_liveness(redis_sock, p) == SUCCESS) {
                     return SUCCESS;
                 }
 