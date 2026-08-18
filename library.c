ebdb75889f64d3686477de7715347501a7dd3a89
michael-grunder <michael.grunder@gmail.com>
2026-08-18 12:53:42 -0700

    fix: Improve GEOSEARCH reply handling
    This fixes a test failure when running against Dragonfly but is also an
    improvement over just using PHPREDIS_CTX_PTR variants to figure out what
    the reply handler should do.

--- library.c
+++ library.c
@@ -1664,14 +1664,78 @@ redis_mbulk_reply_zipped(INTERNAL_FUNCTION_PARAMETERS, RedisSock *redis_sock,
     return 0;
 }
 
-static int
-geosearch_cast(zval *zv)
+static int geosearch_cast_double(zval *zv)
 {
-    if (Z_TYPE_P(zv) == IS_ARRAY) {
-        zend_hash_apply(Z_ARRVAL_P(zv), geosearch_cast);
-    } else if (Z_TYPE_P(zv) == IS_STRING) {
-        convert_to_double(zv);
+    if (Z_TYPE_P(zv) == IS_ARRAY)
+        return FAILURE;
+
+    convert_to_double(zv);
+
+    return SUCCESS;
+}
+
+static int
+geosearch_cast(zval *zv, uintptr_t options)
+{
+    zval *zcoord, *zsub;
+    HashTable *ht;
+    zend_long hash;
+    zend_ulong idx = 1;
+    uint32_t expected = 1;
+
+    expected += (options & REDIS_GEOSEARCH_WITHDIST) != 0;
+    expected += (options & REDIS_GEOSEARCH_WITHHASH) != 0;
+    expected += (options & REDIS_GEOSEARCH_WITHCOORD) != 0;
+
+    if (Z_TYPE_P(zv) != IS_ARRAY ||
+        zend_hash_num_elements(Z_ARRVAL_P(zv)) != expected)
+    {
+        return FAILURE;
     }
+
+    ht = Z_ARRVAL_P(zv);
+    zsub = zend_hash_index_find(ht, 0);
+    if (zsub == NULL || Z_TYPE_P(zsub) == IS_ARRAY)
+        return FAILURE;
+
+    if (options & REDIS_GEOSEARCH_WITHDIST) {
+        zsub = zend_hash_index_find(ht, idx++);
+        if (zsub == NULL || geosearch_cast_double(zsub) == FAILURE)
+            return FAILURE;
+    }
+
+    if (options & REDIS_GEOSEARCH_WITHHASH) {
+        zsub = zend_hash_index_find(ht, idx++);
+        if (zsub == NULL)
+            return FAILURE;
+
+        if (Z_TYPE_P(zsub) == IS_STRING) {
+            if (is_numeric_string(Z_STRVAL_P(zsub), Z_STRLEN_P(zsub),
+                                  &hash, NULL, 0) != IS_LONG)
+            {
+                return FAILURE;
+            }
+            zval_ptr_dtor_nogc(zsub);
+            ZVAL_LONG(zsub, hash);
+        } else if (Z_TYPE_P(zsub) != IS_LONG) {
+            return FAILURE;
+        }
+    }
+
+    if (options & REDIS_GEOSEARCH_WITHCOORD) {
+        zsub = zend_hash_index_find(ht, idx);
+        if (zsub == NULL || Z_TYPE_P(zsub) != IS_ARRAY ||
+            zend_hash_num_elements(Z_ARRVAL_P(zsub)) != 2)
+        {
+            return FAILURE;
+        }
+
+        ZEND_HASH_FOREACH_VAL(Z_ARRVAL_P(zsub), zcoord) {
+            if (geosearch_cast_double(zcoord) == FAILURE)
+                return FAILURE;
+        } ZEND_HASH_FOREACH_END();
+    }
+
     return SUCCESS;
 }
 
@@ -1757,11 +1821,10 @@ redis_mpop_response(INTERNAL_FUNCTION_PARAMETERS, RedisSock *redis_sock,
 
 PHP_REDIS_API int
 redis_read_geosearch_response(zval *zdst, RedisSock *redis_sock,
-                              long long elements, int with_aux_data)
+                              long long elements, uintptr_t options)
 {
     zval z_multi_result, z_sub, *z_ele, *zv;
     zend_string *zkey;
-    int status = SUCCESS;
 
     /* Handle the trivial "empty" result first */
     if (elements < 0 && redis_sock->null_mbulk_as_null) {
@@ -1771,7 +1834,7 @@ redis_read_geosearch_response(zval *zdst, RedisSock *redis_sock,
 
     array_init_size(zdst, elements > 0 ? elements : 0);
 
-    if (with_aux_data == 0) {
+    if (options == 0) {
         redis_mbulk_reply_loop(redis_sock, zdst, elements, UNSERIALIZE_NONE);
     } else {
         array_init_size(&z_multi_result, elements > 0 ? elements : 0);
@@ -1779,29 +1842,18 @@ redis_read_geosearch_response(zval *zdst, RedisSock *redis_sock,
         if (redis_read_multibulk_recursive(redis_sock, elements, 0,
                                            &z_multi_result) == FAILURE)
         {
-            status = FAILURE;
+            goto fail;
         }
 
         ZEND_HASH_FOREACH_VAL(Z_ARRVAL(z_multi_result), z_ele) {
+            if (geosearch_cast(z_ele, options) == FAILURE)
+                goto fail;
+
             // The first item in the sub-array is always the name of the returned item
-            if (Z_TYPE_P(z_ele) != IS_ARRAY) {
-                status = FAILURE;
-                break;
-            }
-
             zv = zend_hash_index_find(Z_ARRVAL_P(z_ele), 0);
-            if (zv == NULL) {
-                status = FAILURE;
-                break;
-            }
-
             zkey = zval_get_string(zv);
 
             zend_hash_index_del(Z_ARRVAL_P(z_ele), 0);
-
-            // The other information is returned in the following order as successive
-            // elements of the sub-array: distance, geohash, coordinates
-            zend_hash_apply(Z_ARRVAL_P(z_ele), geosearch_cast);
 
             // Reindex elements so they start at zero */
             ZVAL_ARR(&z_sub, zend_array_to_list(Z_ARRVAL_P(z_ele)));
@@ -1812,14 +1864,14 @@ redis_read_geosearch_response(zval *zdst, RedisSock *redis_sock,
 
         // Cleanup
         zval_ptr_dtor_nogc(&z_multi_result);
-
-        if (status == FAILURE) {
-            zval_ptr_dtor_nogc(zdst);
-            ZVAL_UNDEF(zdst);
-        }
     }
 
-    return status;
+    return SUCCESS;
+
+fail:
+    zval_ptr_dtor_nogc(&z_multi_result);
+    zval_ptr_dtor_nogc(zdst);
+    return FAILURE;
 }
 
 PHP_REDIS_API int
@@ -1831,7 +1883,7 @@ redis_geosearch_response(INTERNAL_FUNCTION_PARAMETERS, RedisSock *redis_sock,
 
     if (read_mbulk_header(redis_sock, &elements) < 0 ||
         redis_read_geosearch_response(&zret, redis_sock, elements,
-                                      ctx.mode == REDIS_CTX_GEO_WITHMETA) < 0)
+                                      (uintptr_t)ctx.ptr) < 0)
     {
         ZVAL_FALSE(&zret);
     }
